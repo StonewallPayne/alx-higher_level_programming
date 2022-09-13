1c7746a52a923ad07d75f68be5b64735156a93ca
PayneStonewall <paynestonewall@gmail.com>
2022-09-13 16:45:20 -0700

    square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class Square
+
+
+
+    Attributes:
+        __size (int): size of a side of the square
+        __position (tuple): position of the square"""
+
