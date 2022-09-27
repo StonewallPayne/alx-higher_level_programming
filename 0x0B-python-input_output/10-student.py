b670bbd803ed0da048f86cea60412c411f0d212a
PayneStonewall <paynestonewall@gmail.com>
2022-09-27 08:32:34 -0700

    test

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
 """
-Defines "Student" class
+    Module for class Student
 """
 
 
 class Student:
-    """Student"""
+    """
+        A class students that defines a student by:
+        Attributes:
+            first_name (str): name of student.
+            last_name (str): name of student.
+            age (int): age of student.
+        Methods:
+            __init__ - initializes the Student instance.
+            to_json - retrieves dictionary repr of Student instance.
+    """
     def __init__(self, first_name, last_name, age):
-        """Initializes student's information"""
+        """
+            Initialises Student instance.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance
-         with filter"""
-        res = {}
-        if attrs is not None:
-            for a in attrs:
-                try:
-                    res[a] = self.__dict__[a]
-                except:
-                    pass
+    def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+            Args:
+                attr (list): attribute names that are to be retrieved.
+        """
+
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
             return res
-        return self.__dict__
+        else:
+            return self.__dict__
